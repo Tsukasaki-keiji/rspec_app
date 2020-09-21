cd367d916ebe275efde0159cc76b60aaa00643de
EC2 Default User <abcdquont1234@gmail.com>
2020-09-21 13:28:15 +0000

    Update Rubocop

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-context "データが正しく保存される" do
+  context "データが正しく保存される" do
     before do
       @post = Post.new
       @post.title = "Hello WebCamp"
       @post.content = "今日も晴れです。"
       @post.save
     end
+
     it "全て入力してあるので保存される" do
       expect(@post).to be_valid
     end
   end
+
   context "データが正しく保存されない" do
     before do
       @post = Post.new
@@ -19,6 +21,7 @@ context "データが正しく保存される" do
       @post.content = "今日も晴れです。"
       @post.save
     end
+
     it "titleが入力されていないので保存されない" do
       expect(@post).to be_invalid
       expect(@post.errors[:title]).to include("can't be blank")
